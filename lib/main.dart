a30e0945be0cd39bad411ffa6924a40a666b0f9e
rohith <rohith.reddy@codetru.com>
2023-05-26 15:39:31 +0530

    completed the building of the calculator by using the flutter

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_calculator/calculator.dart';
+
+void main() {
+  runApp( MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home :  Calculator(),
+    ));
+}
